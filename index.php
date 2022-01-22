a59e0bd5e5ee7eaa74d122b2525505e9422f224f
wagnergiovanni64 <98218525+wagnergiovanni64@users.noreply.github.com>
2022-01-22 10:20:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="home-wrapper">
+      <div class="container">
+        <div class="card border-0">
+            <h2><span>JOINT RESTORE GUMMIES</span> SPECIAL OFFER...</h2>
+            <hr>
+            <h3>Order TODAY And Save Up To <span>$193!</span></h3>
+          <img class="img-fluid d-block m-auto mid-img" src="img/1.png" alt=""><img class="img-fluid d-block m-auto mid-img" src="img/guarantee.png" alt="">
+            <div class="row">
+                <div class="col-md-6">
+                  <a href="https://go.trkclkz.site/jointrestoregumm424"></a>
+                </div><!-- /.col-md-5 -->
+                <div class="col-md-6"> </div><!-- /.col-md-6 -->
+            </div><!-- /.row -->  
+            <div class="card-footer">
+              <a href="https://go.trkclkz.site/jointrestoregumm424" class="btn"><svg fill="white" class="btn-icon" viewBox="0 0 24 24"><path d="M18.65,2.85L19.26,6.71L22.77,8.5L21,12L22.78,15.5L19.24,17.29L18.63,21.15L14.74,20.54L11.97,23.3L9.19,20.5L5.33,21.14L4.71,17.25L1.22,15.47L3,11.97L1.23,8.5L4.74,6.69L5.35,2.86L9.22,3.5L12,0.69L14.77,3.46L18.65,2.85M9.5,7A1.5,1.5 0 0,0 8,8.5A1.5,1.5 0 0,0 9.5,10A1.5,1.5 0 0,0 11,8.5A1.5,1.5 0 0,0 9.5,7M14.5,14A1.5,1.5 0 0,0 13,15.5A1.5,1.5 0 0,0 14.5,17A1.5,1.5 0 0,0 16,15.5A1.5,1.5 0 0,0 14.5,14M8.41,17L17,8.41L15.59,7L7,15.59L8.41,17Z"></path></svg>
+                &nbsp;Activate My Discount</a>
+            </div>       
+        </div><!-- /.card -->       
+      </div><!-- /.container -->
+    </div><!-- /.home-wrapper -->
+    <footer class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+              <p class="text-center"><a href="privacy-policy.html">Privacy Policy</a><span>|</span><a href="disclaimer.html">Disclaimer</a><span>|</span><a href="contact-us.html">Contact Us</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body><!-- /.body -->
+</html>
